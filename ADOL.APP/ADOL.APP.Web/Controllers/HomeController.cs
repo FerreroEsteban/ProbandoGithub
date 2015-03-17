354dfa53f24781dd2ee55a4d40b1f4a55b299c98
Esteban <ferreroesteban@gmail.com>
2015-03-16 21:21:30 -0300

    Nueva pagina de apuesta de detalle

--- ADOL.APP/ADOL.APP.Web/Controllers/HomeController.cs
+++ ADOL.APP/ADOL.APP.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ADOL.APP.CurrentAccountService.ServiceManager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -21,31 +22,43 @@ namespace ADOL.APP.Web.Controllers
 
         public ActionResult Index()
         {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(baseURL);
-                client.DefaultRequestHeaders.Accept.Clear();
-                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var events = new EventsManager();
+            events.UpdateEvents();
+            //using (var client = new HttpClient())
+            //{
+            //    EventsManager man = new EventsManager();
+            //    man.UpdateEvents();
+            //    man.GetSportEvent("FB");
+            //    client.BaseAddress = new Uri(baseURL);
+            //    client.DefaultRequestHeaders.Accept.Clear();
+            //    //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                // New code:
-                HttpResponseMessage response = client.GetAsync("xml.php?ident=discoverytx&passwd=57t6y67&mgstr=FBARG").Result;
-                if (response.IsSuccessStatusCode)
-                {
-                    List<match> partidos = new List<match>();
+            //    // New code:
+            //    HttpResponseMessage response = client.GetAsync("xml.php?ident=discoverytx&passwd=57t6y67&mgstr=FBARG").Result;
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        List<match> partidos = new List<match>();
 
-                    foreach (XElement partido in response.Content.ReadAsAsync<XElement>().Result.Elements("match"))
-                    {
-                        partidos.Add(new match() { Local = partido.Element("hteam").Value, Visitante = partido.Element("ateam").Value });
-                    }
+            //        foreach (XElement partido in response.Content.ReadAsAsync<XElement>().Result.Elements("match"))
+            //        {
+            //            partidos.Add(new match() { Local = partido.Element("hteam").Value, Visitante = partido.Element("ateam").Value });
+            //        }
 
 
 
-                    ViewBag.Partidos = partidos;
-                }
-            }
+            //        ViewBag.Partidos = partidos;
+            //    }
+            //}
 
             return View();
         }
+
+
+        public ActionResult MatchBet(int? matchId)
+        {
+            return View(matchId);
+        }
+
 
         public ActionResult About()
         {
